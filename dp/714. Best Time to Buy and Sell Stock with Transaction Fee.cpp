86cf2fd145c6943a3b9c6b23185b8edb52e261a5
WJA <wja_1993@163.com>
2018-01-16 17:25:53 +0800

    714

--- dp/714. Best Time to Buy and Sell Stock with Transaction Fee.cpp
+++ dp/714. Best Time to Buy and Sell Stock with Transaction Fee.cpp
@@ -14,7 +14,6 @@ Selling at prices[3] = 8
 Buying at prices[4] = 4
 Selling at prices[5] = 9
 The total profit is ((8 - 1) - 2) + ((9 - 4) - 2) = 8.
-
 Note:
 
 0 < prices.length <= 50000.
@@ -22,5 +21,23 @@ Note:
 0 <= fee < 50000.
 
 
+股票类的问题，在这个题目的discuss里有一个总结贴，写的还是很好的，可以参考一下
+总的思路就是动态规划，其中dp数组为dp[i][k][s], i为第几天，k为几次交易，s为交易之后手上是由股票还是没股票
+根据问题的不同，dp数组可以有一些变化，但是上边这个是普适的。
+*/
 
- */
+
+class Solution {
+public:
+	int maxProfit(vector<int>& prices, int fee) {
+		auto n = prices.size();
+		vector<vector<int>> dp(n, vector<int>(2, 0));
+		dp[0][0] = 0;
+		dp[0][1] = -prices[0];
+		for (int i = 1; i < n; ++i) {
+			dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
+			dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+		}
+		return max(dp[n - 1][0], dp[n - 1][1]);
+	}
+};
